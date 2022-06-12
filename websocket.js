e8872e287ff5f754f42c4a5a5f46053592f81079
Dennis <dennissantosr@gmail.com>
2022-06-12 09:42:16 -0300

    Web socket for conversation chat

---
+++ websocket.js
@@ -0,0 +1,48 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'public'));
+// app.engine('html', require('ejs').renderFile);
+// app.engine('view engine', 'html');
+
+// app.use('/', (req, res) => {
+//     res.render('index.html');
+// });
+
+// server.listen(3000);
+
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, { cors: { origin: "*"}})
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html')
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+server.listen(3000, () => {
+    console.log("Server running...")
+})
+
+let messages = []
+
+io.on('connection', (socket) => {
+    console.log("User connected: " + socket.id)
+
+    socket.emit("previousMessages", messages);
+
+    socket.on("sendMessage", (data) => {
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+        socket.emit('receivedMessage', data)
+    })
+})
